7d8112f961fa26cd8924e0ac076f3d84118677ee
Andreas <andreas21steffens@gmail.com>
2025-07-01 17:09:47 +0200

    gazebo spown

---
+++ launch/jannik_gz.launch.py
@@ -0,0 +1,84 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+import xacro
+
+
+def generate_launch_description():
+
+    # Specify the name of the package and path to xacro file within the package
+    pkg_name = 'jannik'
+    file_subpath = 'description/jannik.xacro'
+
+    # Package Directories
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    pkg_ros_gz_sim_demos = get_package_share_directory('ros_gz_sim_demos')
+
+     # Use xacro to process the file
+    xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
+
+
+    # Robot state publisher
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description_raw}],
+    )
+
+    # Gazebo Sim
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': '-r empty.sdf'}.items(),
+    )
+
+    # RViz
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', os.path.join(pkg_ros_gz_sim_demos, 'rviz', 'joint_states.rviz')],
+    )
+
+    # Spawn
+    spawn = Node(
+        package='ros_gz_sim',
+        executable='create',
+        parameters=[{'name': 'jannik',
+                    'topic': 'robot_description'
+                   }],
+        output='screen',
+    )
+
+    # Gz - ROS Bridge
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            # Clock (IGN -> ROS2)
+            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+            # Joint states (IGN -> ROS2)
+            '/world/empty/model/jannik/joint_state@sensor_msgs/msg/JointState[gz.msgs.ModelJointState',
+        ],
+        remappings=[
+            ('/world/empty/model/jannik/joint_state', 'joint_states'),
+        ],
+        output='screen'
+    )
+
+    return LaunchDescription(
+        [
+            # Nodes and Launches
+            gazebo,
+            spawn,
+            bridge,
+            node_robot_state_publisher,
+            #rviz,
+        ]
+    )
